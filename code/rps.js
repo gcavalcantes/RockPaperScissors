08d5315733ca05823ca3c0fc509a46acf5a23011
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-07-18 19:57:04 -0300

    Applied CSS
    Changed the page by adding CSS.
    Buttons look nice.
    Further changes might be necessary.

--- code/rps.js
+++ code/rps.js
@@ -1,3 +1,5 @@
+// A simple Rock Paper Scissors game implementation
+
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const playerChoice = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
